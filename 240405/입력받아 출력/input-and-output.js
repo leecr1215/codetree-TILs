5c08ed76ec75e5758f27b63c56a62b2619ab5bb4
chelim Lee <leecr1215@naver.com>
2024-04-05 08:42:21 +0900

    Title: 입력받아 출력, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240405/입력받아 출력/input-and-output.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+// const input = fs.readFileSync(0).toString().split('\n');
+
+// let a = input[0];
+// let b = input[1];
+
+// console.log(a, b);
+console.log(fs.readFileSync(0).toString().replace('\n', ' '));
