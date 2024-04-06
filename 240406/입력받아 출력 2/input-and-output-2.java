a9ec08fbe8987c721aeed8657d7db0736a33d14a
chelim Lee <leecr1215@naver.com>
2024-04-06 19:08:32 +0900

    Title: 입력받아 출력 2, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240406/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nums = sc.next().split("-");
+        
+        System.out.printf("%s%s", nums[0], nums[1]);
+
+    }
+}
