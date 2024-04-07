f88a2b8b126bd30c4920016c3cbae4f32aaf3a36
chelim Lee <leecr1215@naver.com>
2024-04-07 09:59:49 +0900

    Title: 두수의 곱과 몫, Time: 244ms, Memory: 14MB, Status: Passed - CodeTree

--- 240407/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 240407/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const [a, b] = fs.readFileSync(0).toString().split(" ").map(Number);
 
 console.log(`${a} * ${b} = ${a * b}`);
-console.log(`${a} / ${b} = ${a / b}`);
+console.log(`${a} / ${b} = ${parseInt(a / b)}`);
