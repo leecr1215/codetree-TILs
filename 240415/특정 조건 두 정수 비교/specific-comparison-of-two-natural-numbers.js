cf8024b3035826f547a9b9d3da2d12a3f2c05830
chelim Lee <leecr1215@naver.com>
2024-04-15 17:07:28 +0900

    Title: 특정 조건 두 정수 비교, Time: 196ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240415/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().split(" ").map(Number);
+
+if(a < b){
+    console.log(1);
+}else{
+    console.log(0);
+}
+
+
+if(a === b){
+    console.log(1);
+}else{
+    console.log(0);
+}
