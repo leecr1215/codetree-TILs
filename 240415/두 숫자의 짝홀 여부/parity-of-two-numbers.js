f3a6045370080e359127820a7ab309e1773090f8
chelim Lee <leecr1215@naver.com>
2024-04-15 17:05:24 +0900

    Title: 두 숫자의 짝홀 여부, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240415/두 숫자의 짝홀 여부/parity-of-two-numbers.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().split(' ').map(Number);
+
+if(a % 2 === 0){
+    console.log('even');
+}else{
+    console.log('odd');
+}
+
+if(b % 2 === 0){
+    console.log('even');
+}else{
+    console.log('odd');
+}
