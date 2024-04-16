603b4c8f463e532efdd0be7b3b3444f1fe2fcce0
chelim Lee <leecr1215@naver.com>
2024-04-16 13:58:23 +0900

    Title: 범위 안의 수, Time: 252ms, Memory: 15MB, Status: Passed - CodeTree

--- 240416/범위 안의 수/number-in-range.js
+++ 240416/범위 안의 수/number-in-range.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const a = Number(fs.readFileSync(0).toString());
 
-if(a >= 10 && a < 20){
+if(a >= 10 && a <= 20){
     console.log('yes');
 }else{
     console.log('no');
