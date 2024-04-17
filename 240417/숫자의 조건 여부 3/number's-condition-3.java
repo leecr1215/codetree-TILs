28a5f3f85ca13809c0bdfc3b1831e69f3b02d215
chelim Lee <leecr1215@naver.com>
2024-04-17 10:19:53 +0900

    Title: 숫자의 조건 여부 3, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240417/숫자의 조건 여부 3/number's-condition-3.java
+++ 240417/숫자의 조건 여부 3/number's-condition-3.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanenr(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
 
