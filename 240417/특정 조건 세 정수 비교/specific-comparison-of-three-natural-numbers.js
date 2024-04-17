56521aac32b8432957d211227c76c885920c1b91
chelim Lee <leecr1215@naver.com>
2024-04-17 09:32:32 +0900

    Title: 특정 조건 세 정수 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240417/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const [a, b, c] = fs.readFileSync(0).toString().map(Number);
+
+let min = Math.min(a, b);
+min = Math.min(min, c);
+
+if(min == a){
+    process.stdout.write(1 + " ");
+}else{
+    process.stdout.write(0 + " ");
+}
+
+if(a == b && b == c){
+    process.stdout.write(1);
+}else{
+    process.stdout.write(0);
+}
