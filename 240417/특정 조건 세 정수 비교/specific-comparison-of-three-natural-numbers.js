0316ec29bbab06d9f7f40bd0578ca371c8706720
chelim Lee <leecr1215@naver.com>
2024-04-17 09:35:43 +0900

    Title: 특정 조건 세 정수 비교, Time: 238ms, Memory: 14MB, Status: Passed - CodeTree

--- 240417/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240417/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const [a, b, c] = fs.readFileSync(0).toString().map(Number);
+const [a, b, c] = fs.readFileSync(0).toString().split(' ').map(Number);
 
 let min = Math.min(a, b);
 min = Math.min(min, c);
@@ -11,8 +11,8 @@ if(min == a){
     process.stdout.write(0 + " ");
 }
 
-if(a == b && b == c){
-    process.stdout.write(1);
+if(a === b && b === c){
+    process.stdout.write('1');
 }else{
-    process.stdout.write(0);
+    process.stdout.write('0');
 }
