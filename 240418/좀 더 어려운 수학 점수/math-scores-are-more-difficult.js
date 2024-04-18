1ba9ad0bdd7042f60ddc6ab8c9e4236a7177d6ca
chelim Lee <leecr1215@naver.com>
2024-04-18 15:11:49 +0900

    Title: 좀 더 어려운 수학 점수, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240418/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const [mathA, englishA, mathB, englishB] = fs.readFileSync(0).toString().split(/s+/).map(Number);
+
+if(mathA === mathB){
+    if(englishA > englishB){
+        console.log('A');
+    }else{
+        console.log('B');
+    }
+}else{
+    if(mathA > mathB){
+        console.log('A');
+    }else{
+        console.log('B');
+    }
+}
