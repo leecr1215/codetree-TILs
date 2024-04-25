3aa4e424e501e15ff90dee27b73450ab00da6732
chelim Lee <leecr1215@naver.com>
2024-04-25 10:02:42 +0900

    Title: 짝수만 출력 2, Time: 173ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240425/짝수만 출력 2/output-only-even-2.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let [b, a] = fs.readFileSync(0).toString().split(" ").map(Number);
+
+while(a <= b){
+    if(b % 2 == 0){
+        process.stdout.write(b + " ");
+    }
+    b--;
+}
