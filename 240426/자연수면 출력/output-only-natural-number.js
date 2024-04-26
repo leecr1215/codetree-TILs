663998dad6b2d6c49ea1ca0cf9c0eca2b1e85ed2
chelim Lee <leecr1215@naver.com>
2024-04-26 11:00:39 +0900

    Title: 자연수면 출력, Time: 216ms, Memory: 14MB, Status: Passed - CodeTree

--- 240426/자연수면 출력/output-only-natural-number.js
+++ 240426/자연수면 출력/output-only-natural-number.js
@@ -4,5 +4,5 @@ const [a, b] = fs.readFileSync(0).toString().split(' ').map(Number);
 if(a > 0){
     process.stdout.write(String(a).repeat(b));
 }else{
-    process.stdout.write(0);
+    process.stdout.write(0+'');
 }
