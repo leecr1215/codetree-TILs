6b1d121d5572829453a665684a93a669b1dd042f
chelim Lee <leecr1215@naver.com>
2024-05-02 12:45:20 +0900

    Title: 윤년 횟수 구하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240502/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(i % 4 == 0){
+                if(!(i % 100 == 0 && i % 400 != 0)){
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
