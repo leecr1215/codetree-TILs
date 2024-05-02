cfd964c139e4d53296041fb4387c4b909aebc521
chelim Lee <leecr1215@naver.com>
2024-05-02 12:34:18 +0900

    Title: 배수의 개수, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240502/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int i = 0; i < 10; i++){
+            int num = sc.nextInt();
+
+            if(num % 3 == 0) cnt1++;
+            if(num % 5 == 0) cnt2++;
+        }
+
+        System.out.println(cnt1 + " " + cnt2);
+    }
+}
