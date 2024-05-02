53ba31cb19a73b1840d8253bea735cb3b6600ba7
chelim Lee <leecr1215@naver.com>
2024-05-02 12:51:33 +0900

    Title: 등장횟수 확인, Time: 192ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240502/등장횟수 확인/check-number-of-appearances.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const nums = fs.readFileSync(0).toString().split('\n').map(Number);
+
+let cnt = 0;
+
+nums.forEach((num)=>{
+    if(num % 2 === 0) cnt++
+});
+
+console.log(cnt);
