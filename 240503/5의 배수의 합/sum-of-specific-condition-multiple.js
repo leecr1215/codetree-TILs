e51719166afcc268df41a6edce740ab39ca8ccf3
chelim Lee <leecr1215@naver.com>
2024-05-03 16:09:45 +0900

    Title: 5의 배수의 합, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

--- 240503/5의 배수의 합/sum-of-specific-condition-multiple.js
+++ 240503/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const [a, b] = fs.readFileSync(0).toString().trim().map(Number);
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 let sum = 0;
 const min = Math.min(a, b);
 const max = Math.max(a, b);
 
 for(let i = min; i <= max; i++){
-    if(i % 5 == 0) sum += i;
+    if(i % 5 === 0) sum += i;
 }
 
 console.log(sum);
