1902e0808cd1fa4417c46683dfbbdfd6653e3085
chelim Lee <leecr1215@naver.com>
2024-05-03 16:09:22 +0900

    Title: 5의 배수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240503/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().trim().map(Number);
+
+let sum = 0;
+const min = Math.min(a, b);
+const max = Math.max(a, b);
+
+for(let i = min; i <= max; i++){
+    if(i % 5 == 0) sum += i;
+}
+
+console.log(sum);
