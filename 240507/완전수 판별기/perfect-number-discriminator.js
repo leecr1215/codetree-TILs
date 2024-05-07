815fec0d05ea7d84e0964fa9a3315d38d9b6c1ec
chelim Lee <leecr1215@naver.com>
2024-05-07 13:14:08 +0900

    Title: 완전수 판별기, Time: 171ms, Memory: 14MB, Status: Passed - CodeTree

--- 240507/완전수 판별기/perfect-number-discriminator.js
+++ 240507/완전수 판별기/perfect-number-discriminator.js
@@ -3,7 +3,7 @@ const n = Number(fs.readFileSync(0).toString().trim());
 
 let sum = 0;
 
-for(let i = 1; i <= Math.ceil(Math.sqrt(n)); i++){
+for(let i = 1; i < n; i++){
     if(n % i == 0){
         sum += i;
     } 
