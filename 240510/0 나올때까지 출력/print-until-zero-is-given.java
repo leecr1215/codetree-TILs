9dd4f269f669fc04c2667676ce98e20daf1a39d3
chelim Lee <leecr1215@naver.com>
2024-05-10 13:24:21 +0900

    Title: 0 나올때까지 출력, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240510/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        while((num = sc.nextInt()) != 0){
+             System.out.println(num);
+        }
+    }
+}
