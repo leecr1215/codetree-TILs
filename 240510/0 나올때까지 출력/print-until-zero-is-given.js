47c84cd3a82af8f40e67d98e5553cbbea0415e27
chelim Lee <leecr1215@naver.com>
2024-05-10 13:22:26 +0900

    Title: 0 나올때까지 출력, Time: 213ms, Memory: 14MB, Status: Passed - CodeTree

--- 240510/0 나올때까지 출력/print-until-zero-is-given.js
+++ 240510/0 나올때까지 출력/print-until-zero-is-given.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const nums = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 
-for(int i = 0; i <= nums.length; i++){
+for(let i = 0; i <= nums.length; i++){
     if(nums[i] === 0) break;
 
     console.log(nums[i]);
