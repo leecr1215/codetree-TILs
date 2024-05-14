f11eefb454fd412b9b4c4825a8e542335b3a4d78
chelim Lee <leecr1215@naver.com>
2024-05-14 09:42:42 +0900

    Title: 몫 출력하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240514/몫 출력하기/print-share.java
+++ 240514/몫 출력하기/print-share.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while(sc.hasNextLine()){
+        int cnt = 0;
+
+        while(cnt < 3){
             int num = sc.nextInt();
 
             if(num % 2 == 0){
                 System.out.println(num / 2);
+                cnt++;
             }
         }
     }
