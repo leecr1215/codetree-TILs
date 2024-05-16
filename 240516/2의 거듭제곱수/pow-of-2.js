115a1f2a6c2717454f53ce7795d2dc6fcf9fe24c
chelim Lee <leecr1215@naver.com>
2024-05-16 11:12:21 +0900

    Title: 2의 거듭제곱수, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240516/2의 거듭제곱수/pow-of-2.js
+++ 240516/2의 거듭제곱수/pow-of-2.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
 const N = Number(fs.readFileSync(0).toString().trim());
 
-console.log(Math.sqrt(N));
+let num = 1;
+let cnt = 0;
+
+while(N != num){
+    num = 2 * num;
+    cnt++;
+}
+
+console.log(cnt);
