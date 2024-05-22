bdd88d51d7e3599ee38623656caf3ed8237fcb4a
chelim Lee <leecr1215@naver.com>
2024-05-22 12:32:28 +0900

    Title: 정사각형 별표 출력하기, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240522/정사각형 별표 출력하기/print-stars-in-square.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+        process.stdout.write('*');
+    }
+    console.log();
+}
