ee4bfcaa4e23b4240582465e2cf1aa3abc404859
chelim Lee <leecr1215@naver.com>
2024-05-22 12:22:04 +0900

    Title: 모두 3의 배수, Time: 240ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240522/모두 3의 배수/all-multiples-of-3.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const nums = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+let result = true;
+
+for(let i = 0; i < 5; i++){
+    if(nums[i] % 3 !== 0) {
+        result = false;
+        break;
+    }
+}
+
+console.log(parseInt(result));
