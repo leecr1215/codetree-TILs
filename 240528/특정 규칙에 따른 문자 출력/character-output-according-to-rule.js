58432959b1a2fa47219c6168d3b7b45218819a16
chelim Lee <leecr1215@naver.com>
2024-05-28 11:01:44 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 185ms, Memory: 47MB, Status: Passed - CodeTree

--- 240528/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
+++ 240528/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -3,17 +3,17 @@ const n = Number(fs.readFileSync(0).toString().trim());
 
 for(let i = 0; i < n; i++){
     for(let j = 0; j < n - i - 1; j++){
-        process.stdout.wrtie("  ");
+        process.stdout.write("  ");
     }
     for(let j = 0; j <= i; j++){
-        process.stdout.wrtie("@ ");
+        process.stdout.write("@ ");
     }
     console.log();
 }
 
 for(let i = n - 1; i > 0; i--){
     for(let j = 0; j < i; j++){
-        process.stdout.wrtie("@ ");
+        process.stdout.write("@ ");
     }
     console.log();
 }
