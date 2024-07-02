e18d9af0036cca186bc908e4d14486ad5c211c14
chelim Lee <leecr1215@naver.com>
2024-07-02 17:01:53 +0900

    Title: 별표 출력하기 12, Time: 298ms, Memory: 14MB, Status: Passed - CodeTree

--- 240702/별표 출력하기 12/print-star-12.js
+++ 240702/별표 출력하기 12/print-star-12.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0).toString().trim());
 
-for(let i = 0; i < n; i++){
+for(let i = 1; i <= n; i++){
     
-    for(let j = 0; j < n; j++){
-        if(j % 2 === 0 && j >= i){
+    for(let j = 1; j <= n; j++){
+        if(j % 2 == 0 && j >= i){
             process.stdout.write("* ");
-        }else if(i === 1){
+        }else if(i == 1){
             process.stdout.write("* ");
         }else{
             process.stdout.write("  ");
