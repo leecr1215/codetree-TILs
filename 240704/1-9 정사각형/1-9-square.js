c3d987da1a27fa7c74521784434ee7720e1ee1a9
chelim Lee <leecr1215@naver.com>
2024-07-04 13:35:29 +0900

    Title: 1-9 정사각형, Time: 234ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240704/1-9 정사각형/1-9-square.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let cnt = 1;
+for(let i = 0; i < n; i++){
+    let str = "";
+    for(let j = 0; j < n; j++){
+        str += cnt++;   
+    }
+    console.log(str);
+}
