043e3aaf83cec7b7961319bb81e5a107009c30fc
chelim Lee <leecr1215@naver.com>
2024-07-05 16:08:58 +0900

    Title: 좌우 지그재그 출력, Time: 266ms, Memory: 12MB, Status: Passed - CodeTree

--- 240705/좌우 지그재그 출력/print-lr-zigzag.java
+++ 240705/좌우 지그재그 출력/print-lr-zigzag.java
@@ -6,23 +6,13 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int evenNum = 1;
-        int oddNum = 2 * n;
-        for(int i = 0; i < n; i++){
-            if(i % 2 == 0){
-                for(int j = 0; j < n; j++){
-                    System.out.print(evenNum+ j + " ");
-                }
-                evenNum += 2 * n;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i % 2 == 0)
+                    System.out.print(((i * n) + j + 1) + " ");
+                else
+                    System.out.print(((i * n) + n - j) + " ");
             }
-
-            else {
-                for(int j = 0; j < n; j++){
-                    System.out.print(oddNum-j + " ");
-                }
-                oddNum += 2 * n;
-            }
-
             System.out.println();
         }
 
