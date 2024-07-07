11ddf46c136eb919614bd0b5efc2f2917d64af4c
chelim Lee <leecr1215@naver.com>
2024-07-07 12:24:39 +0900

    Title: 숫자 피라미드 2, Time: 193ms, Memory: 11MB, Status: Passed - CodeTree

--- 240707/숫자 피라미드 2/number-pyramid-2.java
+++ 240707/숫자 피라미드 2/number-pyramid-2.java
@@ -8,7 +8,7 @@ public class Main {
 
         int cnt = 1;
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < n; i++){
             for(int j = 0; j <= i; j++){
                 System.out.print(cnt++ + " ");
             }
