bd1c71468a43cf79edb8026420ce8c0cc512f0f3
chelim Lee <leecr1215@naver.com>
2024-07-10 15:38:08 +0900

    Title: 19단 출력, Time: 289ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240710/19단 출력/nineteen-times-table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 1; i <= 19; i++){
+            for(int j = 1; j <= 19; j++){
+                if(j % 2 == 0) System.out.printf(" / %d * %d = %d\n", i, j, i * j);
+                else System.out.printf("%d * %d = %d", i, j, i * j);
+            }
+            System.out.println();
+        }
+
+    }
+}
