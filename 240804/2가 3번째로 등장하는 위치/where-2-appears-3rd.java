66b65459398bdda84437bfb71735e6cb563c59cf
chelim Lee <leecr1215@naver.com>
2024-08-04 23:00:14 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240804/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int num = sc.nextInt();
+
+            if(num == 2){
+                cnt++;
+
+                if(cnt == 3){
+                    System.out.println(i+1);
+                }
+            }
+        }
+
+
+        
+    }
+}
