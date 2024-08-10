023c0a0e775f7dbf5f1cb9d661e08f0b55f54c21
chelim Lee <leecr1215@naver.com>
2024-08-10 15:11:29 +0900

    Title: 격자 반대로 채우기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240810/격자 반대로 채우기/grid-anti-fill.java
+++ 240810/격자 반대로 채우기/grid-anti-fill.java
@@ -7,50 +7,30 @@ public class Main {
         int n = sc.nextInt();
         int[][] arr = new int[n][n];
 
-        // 상좌하좌
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, -1, 0, -1};
-
-        int d = 0;
-        int x = n-1;
-        int y = n-1;
-
         int cnt = 1;
-        arr[x][y] = cnt++;
-
-        while(true){
-            int nx = x + dx[d%4];
-            int ny = y + dy[d%4];
-
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n){
-                // 이동 가능
-                x = nx;
-                y = ny;
-
-                arr[x][y] = cnt++;
-                if(cnt > n*n) break;
-                
-
-                // 현재 좌로 이동한 경우
-                if(d % 4 == 1 || d % 4 == 3){
-                    d++;
+        for(int i = n-1; i >= 0; i--){
+             
+            if(i % 2 != 0){
+                for(int j = 0; j < n; j++){
+                    arr[j][i] = cnt++;
                 }
+
             }else{
-                // 튀어나감. 방향 바꿔주기
-                d++;
+                for(int j = n-1; j >= 0; j--){
+                    arr[j][i] = cnt++;
+                }
+
             }
         }
 
         StringBuilder sb = new StringBuilder();
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 sb.append(arr[i][j] + " ");
-            }   
+            }
             sb.append("\n");
         }
-
-        System.out.println(sb.toString());
         
+        System.out.println(sb.toString());
     }
 }
