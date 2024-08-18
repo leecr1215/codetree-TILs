574959f530fc656eea63f836f6ff776372e03881
chelim Lee <leecr1215@naver.com>
2024-08-18 14:45:28 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240818/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240818/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -4,6 +4,6 @@ const input = fs.readFileSync(0).toString().split("\n");
 const strs = input.slice(0, -1);
 const target = input[input.length-1];
 
-strs.filter((str)=>str[str.length-1] === 'e').forEach((str)=>{
+strs.filter((str)=>str[str.length-1] === target).forEach((str)=>{
     console.log(str);
 })
