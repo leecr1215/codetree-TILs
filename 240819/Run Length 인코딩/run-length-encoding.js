48555be501e4fc68e5f2909abcd0b63b189cf7b8
chelim Lee <leecr1215@naver.com>
2024-08-19 17:25:26 +0900

    Title: Run Length 인코딩, Time: 239ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240819/Run Length 인코딩/run-length-encoding.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const strs = fs.readFileSync(0).toString().trim();
+
+let result = '';
+
+let targetAlpha = strs[0];
+let cnt = 0;
+
+for(let i = 0; i < strs.length; i++){
+    if(targetAlpha === strs[i]){
+        cnt++;
+    }else{
+        result += targetAlpha + cnt;
+        targetAlpha = strs[i];
+        cnt = 1;
+    }
+}
+
+if(cnt >= 1){
+    result += targetAlpha + cnt;
+}
+
+console.log(result.length);
+console.log(result);
