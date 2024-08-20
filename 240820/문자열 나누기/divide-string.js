94696d6e3d8fcbf0a7c7a8024f34239db0643137
chelim Lee <leecr1215@naver.com>
2024-08-20 16:04:22 +0900

    Title: 문자열 나누기, Time: 209ms, Memory: 47MB, Status: Passed - CodeTree

--- 240820/문자열 나누기/divide-string.js
+++ 240820/문자열 나누기/divide-string.js
@@ -3,15 +3,13 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const result = input[1].replaceAll(' ', '').split('');
 
 let temp = '';
-let cnt = 0;
 result.forEach((num, idx)=>{
     temp += num;
 
     if((idx+1) % 5 === 0){
         console.log(temp);
         temp = '';
-        cnt++;
     }
 })
 
-console.log(cnt);
+console.log(temp);
