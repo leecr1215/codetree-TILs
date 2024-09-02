dfec5e36e82fa1030860ad05d534020002216b9e
chelim Lee <leecr1215@naver.com>
2024-09-02 17:34:43 +0900

    Title: 문자 받아 출력, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240902/문자 받아 출력/enter-char-and-print.java
+++ 240902/문자 받아 출력/enter-char-and-print.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int c = sc.next();
+        String c = sc.next();
 
         System.out.println(c);
     }
