d15952f31db053422e263ac31626257e669109ce
chelim Lee <leecr1215@naver.com>
2024-09-07 14:09:12 +0900

    Title: 특정 위치의 숫자 2, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240907/특정 위치의 숫자 2/numbers-in-specific-location-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int cnt = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(st.nextToken());
+
+            if(i % 2 != 0){
+                cnt++;
+                sum += num;
+            }
+        }
+
+        System.out.println(sum + " " + Math.floor((double)sum / cnt * 10) / 10);
+    }
+}
