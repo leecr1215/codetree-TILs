11e00b215e877f10fbee7ec631a16875a0261b26
chelim Lee <leecr1215@naver.com>
2024-09-11 23:59:30 +0900

    Title: 실수형 변수 선언하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240911/실수형 변수 선언하기/declare-real-variable.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 12.3;
+        int b = 70;
+
+        System.out.printf("%.2f     %.2f     %.2f", a, (double)b, a+b);
+    }
+}
